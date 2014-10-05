30a389d20b6488fb1d15302b165db7d0fe8044c8
Rafael Rodrigo Silveira <rafaelrodrigosilveira@hotmail.com>
2014-10-04 22:39:40 -0300

    Versão 1.0.11
    Game Controller - controle dos asteroids caindo

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+	public GameObject hazard;
+	public Vector3 spawnValues;
+	
+	void Start ()
+	{
+		SpawnWaves ();
+	}
+	//Funçao que sorteia o local que aparecera os asteroids
+	void SpawnWaves ()
+	{
+		//Random.Rage vai sortear local que aparecera os asteroids
+		Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+		Quaternion spawnRotation = Quaternion.identity;
+		Instantiate (hazard, spawnPosition, spawnRotation);
+	}
+}
