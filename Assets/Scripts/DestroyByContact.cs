0a96a4abfb7cf37c8296f0c2edb5da1660c92271
Rafael Rodrigo Silveira <rafaelrodrigosilveira@hotmail.com>
2014-10-04 21:26:39 -0300

    Versão 1.0.9
    Criando Asteroid e destruindo com tiro

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour
+{
+	//Destroy os Asteroids
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.tag == "Boundary")
+		{
+			return;
+		}
+		Destroy(other.gameObject);
+		Destroy(gameObject);
+	}
+}
