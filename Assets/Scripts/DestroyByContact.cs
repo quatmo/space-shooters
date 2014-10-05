b8a4bf88c06ca41f16c2f1af23bca7b2985c09b3
Rafael Rodrigo Silveira <rafaelrodrigosilveira@hotmail.com>
2014-10-04 22:23:09 -0300

    Versão 1.0.10
    Adicionando efeito de explosão no asteroid e na nave.

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -3,13 +3,24 @@ using System.Collections;
 
 public class DestroyByContact : MonoBehaviour
 {
-	//Destroy os Asteroids
+	public GameObject explosion;
+	public GameObject playerExplosion;
+	
 	void OnTriggerEnter(Collider other) 
 	{
 		if (other.tag == "Boundary")
 		{
 			return;
 		}
+
+		//Explode o objeto (efeito)
+		Instantiate(explosion, transform.position, transform.rotation);
+		if (other.tag == "Player")
+		{
+			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+			//gameController.GameOver ();
+		}
+		//Destroi o objeto que colidiu
 		Destroy(other.gameObject);
 		Destroy(gameObject);
 	}
